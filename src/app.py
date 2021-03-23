dc35bbdd52ccab37532406d6c702cf08b6871167
juliandiazramires <juliancuentaandroid@gmail.com>
2021-03-23 18:54:20 -0500

    first commit

---
+++ src/app.py
@@ -0,0 +1,84 @@
+from flask import Flask, request, jsonify, Response
+from flask.wrappers import Response
+from flask_pymongo import PyMongo
+
+# modulo para cifrar la constraseña y verificar que es correcta
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson import json_util
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config['MONGO_URI']='mongodb://localhost/pythonmongodb'
+mongo = PyMongo(app)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # print(request.json)
+    username = request.json['username']
+    password = request.json['password']
+    email = request.json['email']
+
+    if username and email and password:
+        hashed_password = generate_password_hash(password)
+        id = mongo.db.users.insert(
+            {'username': username, 'email': email, 'password': hashed_password}
+        )
+        response = {
+            'id': str(id),
+            'username': username,
+            'password': hashed_password,
+            'email' : email
+        }
+        return response
+    else:
+        return not_found()
+    return {'message': 'received'}
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = mongo.db.users.find()
+    response = json_util.dumps(users)
+    return Response(response, mimetype='aplication/json')
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    user = mongo.db.users.find_one({'_id': ObjectId(id)})
+    response = json_util.dumps(user)
+    return Response(response, mimetype="application/json")
+
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    mongo.db.users.delete_one({'_id': ObjectId(id)})
+    response = jsonify({'message': 'User ' + id + 'was deleted successfully'})
+    return response
+
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+
+    if username and email and password:
+        hashed_password = generate_password_hash(password)
+        mongo.db.users.update_one({'_id': ObjectId(id)}, {'$set': {
+            'username': username,
+            'password': hashed_password,
+            'email': email
+        }})
+        response = jsonify({'message': 'User '+ id + 'was updated successfully'})
+        return response
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    response = jsonify({
+        'message': 'Resource Not Found ' + request.url,
+        'status': 404
+    })
+    response.status_code = 404
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
